39551d9ba8d2a25e6c2592496d0f5765f0b3aec8
John Whitbeck <john@whitbeck.fr>
2015-12-31 19:48:32 -0800

    Add Apache 2.0 License

--- src/main/java/net/whitbeck/rdb_parser/Eof.java
+++ src/main/java/net/whitbeck/rdb_parser/Eof.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (c) 2015-2016 John Whitbeck. All rights reserved.
+ *
+ * The use and distribution terms for this software are covered by the
+ * Apache License 2.0 (https://www.apache.org/licenses/LICENSE-2.0.txt)
+ * which can be found in the file al-v20.txt at the root of this distribution.
+ * By using this software in any fashion, you are agreeing to be bound by
+ * the terms of this license.
+ *
+ * You must not remove this notice, or any other, from this software.
+ */
+
 package net.whitbeck.rdb_parser;
 
 public class Eof extends Entry {
